e75ea86b0a850dc6f90ad29f213637b45526d87c
Chun-Yuan Chang <left@cmlab.csie.ntu.edu.tw>
2023-01-18 01:15:13 +0000

    production_test

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -3,7 +3,7 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status,
-    amount/100 as amount,
-    created as created_at 
-
-from `dbt-tutorial`.stripe.payment
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+from {{ source('stripe', 'payment') }}
